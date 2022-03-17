875cc11d75717e5edebd4bf0315f1bb90746db29
Sota Ogura <s.ogura.sys21@morijyobi.ac.jp>
2022-03-17 12:05:35 +0900

    BMI出力を2回に変更

--- Bmi.java
+++ Bmi.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Scanner stdin = new Scanner( System.in ) ;
         double BMI;
         BMI = 60 / 1.775 / 1.775;
         System.out.println(BMI);
