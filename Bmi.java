245ad95213065eeb48ff7ba40573ce4f2375ba2d
Sota Ogura <s.ogura.sys21@morijyobi.ac.jp>
2022-03-17 12:16:15 +0900

    BMI出力を2回に変更

--- Bmi.java
+++ Bmi.java
@@ -1,8 +1,11 @@
-public class Main {
+import java.util.*;
+public class Bmi {
     public static void main(String[] args) {
         Scanner stdin = new Scanner( System.in ) ;
-        double BMI;
-        BMI = 60 / 1.775 / 1.775;
+        double BMI , height , weight;
+        weight = stdin.nextInt();
+		height = stdin.nextDouble();
+        BMI = weight / height / height;
         System.out.println(BMI);
         System.out.println(BMI);
     }
